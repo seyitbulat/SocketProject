dc717d16d97f8f925d0304b15ba11b93224ce75b
Seyit Bulat <mseyit68@gmail.com>
2023-11-24 08:19:34 +0300

    added hub

---
+++ SocketProject.Api/Hubs/ChatHub.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SocketProject.Api.Hubs
+{
+	public sealed class ChatHub : Hub
+	{
+		//public override async Task OnConnectedAsync()
+		//{
+		//	await Clients.All.SendAsync("ReceiveMessage", $"{Context.ConnectionId} girdi");
+		//}
+
+
+		public async Task SendMessage(string user, string message, Guid group)
+		{
+			await Clients.Group(group.ToString()).SendAsync("ReceiveMessage", user, message);
+		}
+
+
+		public async Task JoinPrivateChat(string user1, string user2)
+		{
+			string groupName = $"{user1}-{user2}";
+			await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
+		}
+
+
+		public async Task SendMessageToPrivateChat(string user1, string user2, string message)
+		{
+			string groupName = $"{user1}-{user2}";
+			await Clients.Group(groupName).SendAsync(user1, user2, message);
+		}
+	}
+}
--- SocketProject.Api/Program.cs
+++ SocketProject.Api/Program.cs
@@ -1,8 +1,13 @@
+using SocketProject.Api.Hubs;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+builder.Services.AddCors();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddSignalR();
 
 var app = builder.Build();
 
@@ -13,9 +18,12 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseCors();
 
 app.UseAuthorization();
 
+
 app.MapControllers();
+app.MapHub<ChatHub>("/chat");
 
 app.Run();
